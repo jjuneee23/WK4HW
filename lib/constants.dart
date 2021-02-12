3480b06f08c12309b80ca135a6c7aece74fe0685
jjuneee23 <69755868+jjuneee23@users.noreply.github.com>
2021-02-13 00:49:28 +0700

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,61 @@
+const MOVIE_DATA = [
+  {
+    "name":"Bird of Prey",
+    "genre":"Action",
+    "price":2.5,
+    "image":"birdofprey.jpg"
+  },{
+    "name":"Black Panther",
+    "genre":"Action",
+    "price":2.25,
+    "image":"black-panther.jpg"
+  },
+  {
+    "name":"Finding Nemo",
+    "genre":"Cartoon",
+    "price":1.80,
+    "image":"finding-nemo.jpg"
+  },
+  {
+    "name":"Iron Man",
+    "genre":"Action",
+    "price":2.20,
+    "image":"Iron_Man.jpg"
+  },
+  {
+    "name":"I-Tonya",
+    "genre":"Biography",
+    "price":2.50,
+    "image":"i-tonya.jpg"
+  },
+  {
+    "name":"Joker",
+    "genre":"Drama",
+    "price":2.0,
+    "image":"joker.jpg"
+  },
+  {
+    "name":"New Mutant",
+    "genre":"Action",
+    "price":1.8,
+    "image":"new-mutant.jpg"
+  },
+  {
+    "name":"Shutter Island",
+    "genre":"Psychology",
+    "price":2.8,
+    "image":"shutter-island.jpg"
+  },
+  {
+    "name":"The Choice",
+    "genre":"Romance",
+    "price":2.8,
+    "image":"the-choice.jpg"
+  },
+  {
+    "name":"Wonder Woman",
+    "genre":"Action",
+    "price":2.79,
+    "image":"wonderwoman.jpg"
+  }
+];
---
+++ lib/main.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ScrollController controller = ScrollController();
+  bool closeTopContainer = false;
+  double topContainer = 0;
+  double totalAmount = 0.0;
+
+  List<Widget> itemsData = [];
+
+  void getPostsData() {
+    List<dynamic> responseList = MOVIE_DATA;
+    List<Widget> listItems = [];
+    responseList.forEach((post) {
+      listItems.add(Container(
+          height: 200,
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(color: Colors.black.withAlpha(100), blurRadius: 10.0),
+              ]),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      post["name"],
+                      style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0)),
+                    ),
+                    Text(
+                      post["genre"],
+                      style: const TextStyle(fontSize: 17, color: Colors.grey),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "\$ ${post["price"]}",
+                      style: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      child: RaisedButton(
+                        onPressed: () {
+                          setState(() {
+                            totalAmount = totalAmount + post["price"];
+                          });
+                        },
+                        child: new Text('Add'),
+                      ),
+                    ),
+                  ],
+                ),
+                Image.asset(
+                  "assets/images/${post["image"]}",
+                  height: double.infinity,
+                )
+              ],
+            ),
+          )));
+    });
+    setState(() {
+      itemsData = listItems;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getPostsData();
+    controller.addListener(() {
+      double value = controller.offset / 180;
+
+      setState(() {
+        topContainer = value;
+        closeTopContainer = controller.offset > 50;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search, color: Colors.black),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.person, color: Colors.black),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          height: size.height,
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Text(
+                    "Total: ",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 30),
+                  ),
+                  Text(
+                    "\$${getTotalAmount(totalAmount)}",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.normal,
+                        fontSize: 30),
+                  ),
+                  Container(
+                    child: RaisedButton(
+                      onPressed: () {
+                        setState(() {
+                          totalAmount = 0;
+                        });
+                      },
+                      child: new Text('Clear'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Expanded(
+                  child: ListView.builder(
+                      controller: controller,
+                      itemCount: itemsData.length,
+                      physics: BouncingScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        double scale = 1.0;
+                        if (topContainer > 0.5) {
+                          scale = index + 0.5 - topContainer;
+                          if (scale < 0) {
+                            scale = 0;
+                          } else if (scale > 1) {
+                            scale = 1;
+                          }
+                        }
+                        return Opacity(
+                          opacity: scale,
+                          child: Transform(
+                            transform: Matrix4.identity()..scale(scale, scale),
+                            alignment: Alignment.bottomCenter,
+                            child: Align(
+                                heightFactor: 0.7,
+                                alignment: Alignment.topCenter,
+                                child: itemsData[index]),
+                          ),
+                        );
+                      })),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+String getTotalAmount(double totalAmount) {
+  return totalAmount.toStringAsFixed(2);
+}
